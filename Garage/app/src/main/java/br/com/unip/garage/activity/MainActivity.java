44fce69d19d922af241202775b018b2cb8d4fc02
Caique Amaral <caiqu.7@gmail.com>
2016-11-04 21:25:26 -0200

    Criada transição da tela home pra creditos

--- Garage/app/src/main/java/br/com/unip/garage/activity/MainActivity.java
+++ Garage/app/src/main/java/br/com/unip/garage/activity/MainActivity.java
@@ -1,8 +1,11 @@
 package br.com.unip.garage.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import br.com.unip.garage.R;
 
@@ -14,5 +17,14 @@ public class MainActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        Button botaoCreditos = (Button) findViewById(R.id.botaoMenu);
+        botaoCreditos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startCreditos = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(startCreditos);
+            }
+        });
     }
 }
