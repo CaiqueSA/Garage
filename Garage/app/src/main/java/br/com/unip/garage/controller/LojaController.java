2c11542223c1c2f000c51bd0d2a6676b39adcbe3
Caique Amaral <caiqu.7@gmail.com>
2016-11-05 02:51:11 -0200

    Criado classe controller para loja e enumeration para as pecas

---
+++ Garage/app/src/main/java/br/com/unip/garage/controller/LojaController.java
@@ -0,0 +1,9 @@
+package br.com.unip.garage.controller;
+
+/**
+ * Created by caique on 05/11/16.
+ * Esta classe é responsavél pela lógica da loja {@link br.com.unip.garage.activity.LojaActivity}
+ */
+
+public class LojaController {
+}
---
+++ Garage/app/src/main/java/br/com/unip/garage/enumeration/TipoPeca.java
@@ -0,0 +1,10 @@
+package br.com.unip.garage.enumeration;
+
+/**
+ * Created by caique on 05/11/16.
+ */
+
+public enum TipoPeca {
+
+    PNEU, TURBO, MOTOR, FREIO, PISTAO;
+}
