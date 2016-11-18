4cbac224bcf436e22583cee3b63c7530e9f88ce7
Caique Amaral <caiqu.7@gmail.com>
2016-11-18 07:34:16 -0200

    Inicio da tela de mapas

---
+++ Garage/app/src/main/java/br/com/unip/garage/activity/MapaActivity.java
@@ -0,0 +1,15 @@
+package br.com.unip.garage.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.unip.garage.R;
+
+public class MapaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapa);
+    }
+}
